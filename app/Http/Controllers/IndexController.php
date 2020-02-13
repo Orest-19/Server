3057396ef4ad2c53518635caf881f755ace5951c
Orest-19 <60379541+Orest-19@users.noreply.github.com>
2020-02-13 09:24:11 +0200

    comm

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,6 +8,9 @@ class IndexController extends Controller
 {
     public function index() {
     	$hl = 'Hello, world!';
-    	return view('index')->with('hl',$hl);
+    	$par = 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
+    	return view('index')->with(['hl'=>$hl,
+    							    'par'=>$par]);
+    	
     }
 }
